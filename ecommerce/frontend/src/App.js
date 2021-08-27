a98d3c398ce85d2659acba062879591b40a0b777
jeffbat <75343055+jeffbat@users.noreply.github.com>
2021-08-27 14:34:11 -0400

    first push

---
+++ ecommerce/frontend/src/App.js
@@ -0,0 +1,24 @@
+import { Container } from 'react-bootstrap'
+import logo from './logo.svg';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import HomeScreen from './screens/HomeScreen'
+
+
+
+
+function App() {
+  return (
+    <div>
+        <Header />
+        <main>
+            <Container>
+                <HomeScreen />
+            </Container>
+        </main>
+        <Footer />
+    </div>
+  );
+}
+
+export default App;
